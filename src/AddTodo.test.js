fabd117245b318794ef543b4384c81744802ae39
Daniel Zhang <ddz1@williams.edu>
2022-06-26 11:24:47 -0400

    done

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -19,25 +19,95 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "dtest"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "duplicate test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
 
- test('test that App component doesn\'t add a task without task name', () => {
+  const check = screen.getAllByText(/dtest/i);
+  expect(check.length).toBe(1);
+});
+  
+
+test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
 
- test('test that App component doesn\'t add a task without due date', () => {
+  fireEvent.change(inputTask, { target: { value: ""}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+
+  /**const emptyList = "You have no todo's left";
+  const check = screen.getByText(new RegExp(emptyList, "i"));
+  expect(check).toBeInTheDocument();**/
+
+});
+
+test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "";
+  
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+
+  /**const emptyList = "You have no todo's left";
+  const check = screen.getByText(new RegExp(emptyList, "i"));
+  expect(check).toBeInTheDocument();**/
+});
 
 
 
- test('test that App component can be deleted thru checkbox', () => {
+test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+  
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+});
 
 
- test('test that App component renders different colors for past due events', () => {
+test('test that App component renders different colors for past due events', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2022";
+  
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  
+  const check = screen.getByTestId(/test task/i).style.background;
+  expect(check).toBe("rgb(255, 204, 203)");
+});
